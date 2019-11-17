8e461896e785094fce87f2172ed5b9b94337d713
mervinkogei <vincent.kogei@yahoo.com>
2019-11-17 22:58:57 +0300

    update the HeroTest to return null when no search result is not found

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -7,43 +7,43 @@ public class HeroTest {
     @Test
     public void createsInstanceOfHero()
     {
-        Hero hero = new Hero("Hulk",30,"Strength","Anger issues");
+        Hero hero = new Hero("Kogei",24,"Strength","Giving up easily");
         assertTrue(hero instanceof Hero);
     }
 
     @Test
     public void savesName()
     {
-        Hero hero = new Hero("Hulk",30,"Strength","Anger issues");
+        Hero hero = new Hero("Kogei",24,"Strength","Giving up easily");
         assertEquals("Hulk",hero.getName());
     }
 
     @Test
     public void savesAge()
     {
-        Hero hero = new Hero("Hulk",30,"Strength","Anger issues");
+        Hero hero = new Hero("Kogei",24,"Strength","Giving up easily");
         assertEquals(30,hero.getAge());
     }
 
     @Test
     public void saveSpecialPowers()
     {
-        Hero hero = new Hero("Hulk",30,"Strength","Anger issues");
+        Hero hero = new Hero("Kogei",24,"Strength","Giving up easily");
         assertEquals("Strength",hero.getSpecial_power());
     }
 
     @Test
     public void savesWeakness()
     {
-        Hero hero = new Hero("Hulk",30,"Strength","Anger issues");
+        Hero hero = new Hero("Kogei",24,"Strength","Giving up easily");
         assertEquals("Anger issues",hero.getWeakness());
     }
 
     @Test
     public void all_returns_all_instancesOfHero()
     {
-        Hero hero = new Hero("Hulk1",40,"Strength","Anger issues");
-        Hero hero1 = new Hero("Hulk",30,"Strength","Anger issues");
+        Hero hero = new Hero("Kogei Junior",25,"Strength","Giving up easily");
+        Hero hero1 = new Hero("Kogei",24,"Strength","Giving up easily");
         assertTrue(Hero.all().contains(hero));
         assertTrue(Hero.all().contains(hero1));
     }
@@ -57,22 +57,22 @@ public class HeroTest {
     @Test
     public void getId_heroInstantiateWithAnId_1() {
         Hero.clear();
-        Hero hero = new Hero("Hulk1",40,"Strength","Anger issues");
+        Hero hero = new Hero("Kogei",40,"Strength","Giving up easily");
         assertEquals(1,hero.getId());
     }
 
     @Test
     public void find_returnsCategoryWithSameId_secondCategory() {
         Hero.clear();
-        Hero hero = new Hero("Hulk1",40,"Strength","Anger issues");
+        Hero hero = new Hero("Kogei",40,"Strength","Giving up easily");
         assertEquals(Hero.find(hero.getId()), hero);
     }
 
     @Test
     public void find_hero_by_name() {
         Hero.clear();
-        Hero hero = new Hero("Hulk1",40,"Strength","Anger issues");
-        assertTrue(Hero.findHeroByName("Hulk1"));
+        Hero hero = new Hero("Kogei Junior",25,"Strength","Giving up easily");
+        assertTrue(Hero.findHeroByName("Kogei Junior"));
     }
 
     @Test
