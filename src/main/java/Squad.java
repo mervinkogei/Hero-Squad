af4efeb7d75b8458545c720ecb01af95638aa37e
mervinkogei <vincent.kogei@yahoo.com>
2019-11-17 22:38:43 +0300

    create Squad class and declare the instatances

---
+++ src/main/java/Squad.java
@@ -0,0 +1,17 @@
+public class Squad {
+    private int max_size;
+    private String name;
+    private String cause;
+    private static List<Squad> instances = new ArrayList<Squad>();
+    private int id;
+    private List<Hero> heroes;
+
+    public Squad(int max_size, String name, String cause) {
+        this.max_size = max_size;
+        this.name = name;
+        this.cause = cause;
+        instances.add(this);
+        id = instances.size();
+        heroes = new ArrayList<Hero>();
+    }
+}
