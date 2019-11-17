44cde8a29a5dd02e164ed46e26fd64ccd40b1f42
mervinkogei <vincent.kogei@yahoo.com>
2019-11-17 23:08:48 +0300

    update the SquadTest to add check ArrayList to be true

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,34 +1,39 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
 public class SquadTest {
     @Test
     public void creates_instanceOfSquad()
     {
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
         assertTrue(squad instanceof Squad);
     }
     @Test
     public void saves_maxSize_5()
     {
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
         assertEquals(5, squad.getMax_size());
     }
     @Test
-    public void saves_squadName_squad_1()
+    public void saves_squadName_squad_team_A()
     {
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
-        assertEquals("Squad 1", squad.getName());
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
+        assertEquals("Squad Team_A", squad.getName());
     }
     @Test
     public void saves_cause_black_lives_matterl()
     {
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
         assertEquals("black lives matter", squad.getCause());
     }
 
     @Test
     public void all_returns_all_instancesOfSquad()
     {
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
-        Squad squad2 = new Squad(5,"Squad 1","black lives matter");
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
+        Squad squad2 = new Squad(5,"Squad Team_A","black lives matter");
         assertTrue(Squad.all().contains(squad));
         assertTrue(Squad.all().contains(squad2));
 
@@ -43,28 +48,28 @@ public class SquadTest {
     @Test
     public void getId_SquadInstantiateWithAnId_1() {
         Squad.clear();
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
         assertEquals(1,squad.getId());
     }
 
     @Test
     public void find_returnsCategoryWithSameId_secondCategory() {
         Squad.clear();
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
         assertEquals(Squad.find(squad.getId()), squad);
     }
 
     @Test
     public void getSquad_initiallyReturnsEmptyList_ArrayList() {
         Squad.clear();
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
         assertEquals(0, squad.getHeroes().size());
     }
 
     @Test
     public void addsHeroesToList_true() {
-        Squad squad = new Squad(5,"Squad 1","black lives matter");
-        Hero hero = new Hero("Hulk",30,"Strength","Anger issues");
+        Squad squad = new Squad(5,"Squad Team_A","black lives matter");
+        Hero hero = new Hero("Kogei",30,"Strength","Giving up Easily");
         squad.addHero(hero);
         assertTrue(squad.getHeroes().contains(hero));
     }
